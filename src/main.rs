3cc7b99f8e6e96ad2b39e21d23d9103cfb5aff1b
tarithj <tarithj@gmail.com>
2021-03-23 13:06:56 +0530

    added support for info logging in log files

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,21 @@
 use crate::vm::VM;
 use std::env;
+use simplelog::*;
+use std::fs::File;
 
 pub mod vm;
 pub mod instructions;
 
 fn main() {
+    CombinedLogger::init(
+        vec![
+            TermLogger::new(LevelFilter::Warn, Config::default(), TerminalMode::Mixed),
+            TermLogger::new(LevelFilter::Error, Config::default(), TerminalMode::Mixed),
+            WriteLogger::new(LevelFilter::Info, Config::default(), File::create("perling.info.log").unwrap()),
+            WriteLogger::new(LevelFilter::Error, Config::default(), File::create("perling.info.log").unwrap()),
+        ]
+    ).unwrap();
+
     let args: Vec<String> = env::args().collect();
 
     use std::io::Read;
