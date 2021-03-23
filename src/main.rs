d5d482a4bb7196133e243c104b3b47f222762b4e
tarithj <tarithj@gmail.com>
2021-03-23 14:47:11 +0530

    added new process used register count

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,40 @@
 use crate::vm::VM;
-use std::env;
+use log::info;
 use simplelog::*;
+use std::env;
 use std::fs::File;
 
-pub mod vm;
 pub mod instructions;
+pub mod vm;
 
 fn main() {
-    CombinedLogger::init(
-        vec![
-            TermLogger::new(LevelFilter::Warn, Config::default(), TerminalMode::Mixed),
-            TermLogger::new(LevelFilter::Error, Config::default(), TerminalMode::Mixed),
-            WriteLogger::new(LevelFilter::Info, Config::default(), File::create("perling.info.log").unwrap()),
-            WriteLogger::new(LevelFilter::Error, Config::default(), File::create("perling.info.log").unwrap()),
-        ]
-    ).unwrap();
+    CombinedLogger::init(vec![
+        TermLogger::new(LevelFilter::Warn, Config::default(), TerminalMode::Mixed),
+        TermLogger::new(LevelFilter::Error, Config::default(), TerminalMode::Mixed),
+        WriteLogger::new(
+            LevelFilter::Info,
+            Config::default(),
+            File::create("perling.info.log").unwrap(),
+        ),
+        WriteLogger::new(
+            LevelFilter::Error,
+            Config::default(),
+            File::create("perling.info.log").unwrap(),
+        ),
+    ])
+    .unwrap();
 
     let args: Vec<String> = env::args().collect();
 
     use std::io::Read;
-    let file = std::fs::File::open(args[1].to_string()).unwrap().bytes().map(|ch| ch.unwrap());
+    let file = std::fs::File::open(args[1].to_string())
+        .unwrap()
+        .bytes()
+        .map(|ch| ch.unwrap());
     let mut vm = VM::new();
     for i in file {
-        vm.program.append(&mut vec!(i));
+        vm.program.append(&mut vec![i]);
     }
     vm.run();
+    info!("process used {} register(s)", vm.get_register_usage());
 }
