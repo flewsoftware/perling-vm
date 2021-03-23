f3f194339708a5b967077ce0942a15a2d1dde080
tarithj <tarithj@gmail.com>
2021-03-23 13:09:36 +0530

    added new syscall(exit)

--- src/vm.rs
+++ src/vm.rs
@@ -1,4 +1,5 @@
 use crate::instructions::Opcode;
+use log::info;
 
 #[derive(Debug)]
 pub struct VM {
@@ -6,7 +7,7 @@ pub struct VM {
     pub program_counter: usize,
     pub program: Vec<u8>,
     pub remainder: u32,
-    pub program_set_counter: i32
+    pub program_set_counter: i32,
 }
 
 impl VM {
@@ -30,24 +31,24 @@ impl VM {
 
     /// returns the next 16 bits and increments the program counter
     fn next_16_bits(&mut self) -> u16 {
-        let result = ((self.program[self.program_counter] as u16) << 8) | self.program[self.program_counter + 1] as u16;
+        let result = ((self.program[self.program_counter] as u16) << 8)
+            | self.program[self.program_counter + 1] as u16;
         self.program_counter += 2;
         return result;
     }
 
-
     // internal functions
     /// decodes opcode to Opcode object
-    fn decode_opcode(&mut self) -> Opcode {
-        let opcode = Opcode::from(self.program[self.program_counter]);
+    fn decode_opcode(&mut self) -> (Opcode, u8) {
+        let original_op = self.program[self.program_counter];
+        let opcode = Opcode::from(original_op);
         self.program_counter += 1;
-        return opcode;
+        return (opcode, original_op);
     }
 
     /// executes VM call
-    pub fn execute_vm_call(&mut self, call_name: i32, arg1: i32, arg2: i32) {
+    pub fn execute_vm_call(&mut self, call_name: i32, arg1: i32, arg2: i32) -> (bool, i32) {
         match call_name {
-
             // print call
             0 => {
                 // print mode
@@ -58,89 +59,113 @@ impl VM {
                 }
             }
 
+            // exit call
+            1 => {
+                return (false, arg2);
+            }
             _ => {
-                panic!("Invalid VM call, {}  at program set: {} program counter: {} ", call_name, self.program_set_counter, self.program_counter)
+                panic!(
+                    "Invalid VM call, {}  at program set: {} program counter: {} ",
+                    call_name, self.program_set_counter, self.program_counter
+                )
             }
         }
+        return (true, 0);
     }
 
     // execution functions
     /// Loops as long as instructions can be executed.
     pub fn run(&mut self) {
-        let mut is_done = false;
-        while !is_done {
-            is_done = self.execute_instruction();
+        loop {
+            let (dont_kill, code) = self.execute_instruction();
+            if !dont_kill {
+                println!("process exited with code: {}", code);
+                break;
+            }
         }
     }
-
 
     /// Executes one instruction. Meant to allow for more controlled execution of the VM
     pub fn run_once(&mut self) {
         self.execute_instruction();
     }
 
-    pub fn execute_instruction(&mut self) -> bool {
+    pub fn execute_instruction(&mut self) -> (bool, i32) {
+        let (decoded_op, code) = self.decode_opcode();
+        info!("got new instruction {}", code);
         if self.program_counter >= self.program.len() {
-            return true;
+            return (false, 0);
         }
-        match self.decode_opcode() {
+        match decoded_op {
             Opcode::LOAD => {
                 let register = self.next_8_bits() as usize;
                 let number = self.next_16_bits() as u32;
+                info!("Loading {} to R{}", number, register);
                 // loads the number into the register
                 self.registers[register] = number as i32;
-            },
+            }
             Opcode::HLT => {
                 println!("HLT encountered");
-                return true;
-            },
+                return (true, 0);
+            }
             Opcode::ADD => {
                 let register1 = self.registers[self.next_8_bits() as usize];
                 let register2 = self.registers[self.next_8_bits() as usize];
+                let output_register = self.next_8_bits() as usize;
+                info!(
+                    "adding {} + {} to R{}",
+                    register1, register2, output_register
+                );
                 // loads the sum of register 1 & 2 into the
-                self.registers[self.next_8_bits() as usize] = register1 + register2;
-            },
+                self.registers[output_register] = register1 + register2;
+            }
             Opcode::SUB => {
                 let register1 = self.registers[self.next_8_bits() as usize];
                 let register2 = self.registers[self.next_8_bits() as usize];
+                let output_register = self.next_8_bits() as usize;
+                info!(
+                    "adding {} - {} to R{}",
+                    register1, register2, output_register
+                );
                 // loads the subtraction of register 1 & 2 into the
-                self.registers[self.next_8_bits() as usize] = register1 - register2;
-            },
+                self.registers[output_register] = register1 - register2;
+            }
             Opcode::DIV => {
                 let register1 = self.registers[self.next_8_bits() as usize];
                 let register2 = self.registers[self.next_8_bits() as usize];
                 self.registers[self.next_8_bits() as usize] = register1 / register2;
                 self.remainder = (register1 % register2) as u32;
-            },
+            }
             Opcode::JMP => {
                 let target = self.registers[self.next_8_bits() as usize];
                 self.program_counter = target as usize;
-                return false;
-            },
+                return (true, 0);
+            }
             Opcode::RJMP => {
                 let value = self.registers[self.next_8_bits() as usize];
                 self.program_counter += value as usize;
-                return false;
-            },
+                return (true, 0);
+            }
             Opcode::VMCALL => {
                 let call_name = self.registers[self.next_8_bits() as usize];
                 let arg1 = self.registers[self.next_8_bits() as usize];
                 let arg2 = self.registers[self.next_8_bits() as usize];
-                self.execute_vm_call(call_name, arg1, arg2);
-            },
+                info!("executing VMCALL {} {} {}", call_name, arg1, arg2);
+                return self.execute_vm_call(call_name, arg1, arg2); // returns false if kill
+            }
             _ => {
-                println!("Unknown opcode at program set: {} program counter: {}", self.program_set_counter, self.program_counter);
-                return true;
-            },
+                println!(
+                    "Unknown opcode at program set: {} program counter: {}",
+                    self.program_set_counter, self.program_counter
+                );
+                return (false, 4);
+            }
         }
         self.program_set_counter += 1;
         self.program_counter = (self.program_set_counter as usize) * (4 as usize);
-        false
+        return (true, 0);
     }
 }
-
-
-
 
 #[cfg(test)]
 mod tests {
@@ -156,7 +181,7 @@ mod tests {
     fn test_opcode_hlt() {
         // tests if vm will halt when no instructions
         let mut test_vm = VM::new();
-        let test_bytes = vec![0,0,0,0];
+        let test_bytes = vec![0, 0, 0, 0];
         test_vm.program = test_bytes;
         test_vm.run();
         assert_eq!(test_vm.program_counter, 1);
@@ -165,7 +190,7 @@ mod tests {
     #[test]
     fn test_opcode_igl() {
         let mut test_vm = VM::new();
-        let test_bytes = vec![200,0,0,0];
+        let test_bytes = vec![200, 0, 0, 0];
         test_vm.program = test_bytes;
         test_vm.run();
         assert_eq!(test_vm.program_counter, 1);
@@ -182,7 +207,7 @@ mod tests {
     #[test]
     fn test_add_opcode() {
         let mut test_vm = VM::new();
-        test_vm.program = vec![1, 0, 1, 244,  1,1,1,244, 2,0,1,2]; // load(opcode: 1) 500 into register 0
+        test_vm.program = vec![1, 0, 1, 244, 1, 1, 1, 244, 2, 0, 1, 2]; // load(opcode: 1) 500 into register 0
         test_vm.run();
 
         assert_eq!(test_vm.registers[2], 1000)
@@ -218,7 +243,7 @@ mod tests {
     fn test_vmcall_print_opcode() {
         let mut test_vm = VM::new();
         test_vm.registers[0] = 2;
-        test_vm.program = vec![1,0,1,255, 1,1,0,0, 8,1,0,0, 8,1,0,0];
+        test_vm.program = vec![1, 0, 1, 255, 1, 1, 0, 0, 8, 1, 0, 0, 8, 1, 0, 0];
         test_vm.run();
     }
 }
