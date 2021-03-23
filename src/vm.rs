d09c2aa2f3b695398c40ea6e68632ad95c44072f
tarithj <tarithj@gmail.com>
2021-03-23 15:54:16 +0530

    added tests for NEQ opcode

--- src/vm.rs
+++ src/vm.rs
@@ -338,4 +338,20 @@ mod tests {
         test_vm.run_once();
         assert_eq!(test_vm.program_counter, 4);
     }
+
+    #[test]
+    fn test_neq_opcode() {
+        let mut test_vm = VM::new();
+        test_vm.registers[0] = 10;
+        test_vm.registers[1] = 10;
+        test_vm.program = vec![11, 0, 1, 3];
+        test_vm.run();
+        assert_eq!(test_vm.registers[3], 0 as i32);
+        test_vm.reset_program();
+        test_vm.registers[1] = 25;
+        test_vm.run();
+        assert_eq!(test_vm.registers[3], 1 as i32);
+    }
+
+
 }
