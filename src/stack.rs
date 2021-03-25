0c5327ec5984cc21b9f7d5f1310acc0c126ae3c6
Tarith <tarithj@gmail.com>
2021-03-25 21:02:26 +0530

    created stack.rs

--- src/stack.rs
+++ src/stack.rs
@@ -1,3 +1,43 @@
+use crate::register;
+use std::cmp::PartialEq;
+use std::ops::Add;
+
+#[derive(Debug, PartialEq)]
 pub struct STACK {
-    pub contents: Vec<i32>,
+    pub content: Vec<i32>,
+}
+
+impl Add<register::REGISTER> for STACK {
+    type Output = STACK;
+
+    fn add(self, other: register::REGISTER) -> STACK {
+        let mut temp_content = self.content;
+        temp_content.push(other.content);
+        return STACK {
+            content: temp_content,
+        };
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_add_register_method() {
+        let test_register = register::REGISTER {
+            content: 2,
+            locked: false,
+        };
+        let test_stack = STACK {
+            content: vec![3, 5],
+        };
+        assert_eq!(
+            test_stack + test_register + test_register,
+            STACK {
+                content: vec![3, 5, 2, 2],
+            },
+        );
+    }
+
 }
