b0560a6f8775693feb7036e8485fee9665f62682
tarithj <tarithj@gmail.com>
2021-04-16 14:15:28 +0530

    created label.rs and LABEL struct

---
+++ src/label.rs
@@ -0,0 +1,10 @@
+#[derive(Debug)]
+/// stores information about a label
+pub struct LABEL {
+
+    /// id of the LABEL
+    pub id: i32,
+
+    /// location(program_set_counter value) of the label
+    pub location: i32,
+}
